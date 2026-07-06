95f71ac097f9bac10e4b99ebed807de603525c4b
Zhouwen-CN <597879949@qq.com>
2026-07-06 09:07:04 +0800

    feat: 删除过期的任务实例数据

--- src/main/java/com/etl/api/scheduler/DeleteExpirationRecord.java
+++ src/main/java/com/etl/api/scheduler/DeleteExpirationRecord.java
@@ -1,7 +1,11 @@
 package com.etl.api.scheduler;
 
+import com.etl.api.domain.entity.EtlJobInstance;
 import com.etl.api.domain.entity.HttpExchangeHistory;
 import com.etl.api.domain.entity.LoginCaptcha;
+import com.etl.api.enumeration.ETLJobTypeEnum;
+import com.etl.api.enumeration.FlinkJobStatusEnum;
+import com.etl.api.service.EtlJobInstanceService;
 import com.etl.api.service.HttpExchangeHistoryService;
 import com.etl.api.service.LoginCaptchaService;
 import lombok.RequiredArgsConstructor;
@@ -22,15 +26,18 @@ public class DeleteExpirationRecord {
 
     private final LoginCaptchaService loginCaptchaService;
     private final HttpExchangeHistoryService httpExchangeHistoryService;
+    private final EtlJobInstanceService etlJobInstanceService;
     @Value("${custom.captcha.expiration}")
     private Duration captchaExpiration;
     @Value("${custom.http-exchange.expiration}")
     private Duration httpExchangeExpiration;
+    @Value("${custom.job-instance.expiration}")
+    private Duration jobInstanceExpiration;
 
     /*
      * 定时删除过期数据
      * */
-    @Scheduled(fixedDelay = 30, timeUnit = TimeUnit.MINUTES)
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
     public void run() {
         log.debug("删除 验证码、请求历史 过期数据");
 
@@ -42,6 +49,13 @@ public class DeleteExpirationRecord {
         // 删除过期的请求历史
         httpExchangeHistoryService.updateChain()
                 .lt(HttpExchangeHistory::getTimestamp, System.currentTimeMillis() - httpExchangeExpiration.toMillis())
+                .remove();
+
+        // 删除过期的任务实例
+        etlJobInstanceService.updateChain()
+                .eq(EtlJobInstance::getJobType, ETLJobTypeEnum.BATCH.getCode())
+                .notIn(EtlJobInstance::getStatus, FlinkJobStatusEnum.getProcessingStatus())
+                .lt(EtlJobInstance::getEndTime, LocalDateTime.now().minus(jobInstanceExpiration.toMillis(), ChronoUnit.MILLIS))
                 .remove();
     }
 
