d2c93fe34eee0cd931fec0ad2c4a43a66469d57f
Zhouwen-CN <597879949@qq.com>
2026-09-22 15:32:56 +0800

    feat: flink restClient 的指标名称，使用默认值，方便监控

--- src/main/java/com/etl/api/config/RestClientConfiguration.java
+++ src/main/java/com/etl/api/config/RestClientConfiguration.java
@@ -3,7 +3,6 @@ package com.etl.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.client.observation.DefaultClientRequestObservationConvention;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -12,7 +11,7 @@ public class RestClientConfiguration {
     @Bean
     public RestClient restClient(RestClient.Builder builder) {
         return builder
-                .observationConvention(new DefaultClientRequestObservationConvention("flink.api.requests"))
+                // .observationConvention(new DefaultClientRequestObservationConvention("flink.api.requests"))
                 .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic ZmxpbmthZG1pbjpBdSEyUGojQU1wc2M=")
                 .build();
     }
